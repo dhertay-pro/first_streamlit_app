c00ad80c051ad53fb0fb0ac3ee51fc8a4468b325
dhertay-pro <112101012+dhertay-pro@users.noreply.github.com>
2022-08-25 17:44:01 +0200

    Update streamlit_app.py
    mistake change

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc(fruits_selected)
-streamlit.dataframe(fruit_to_show)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
